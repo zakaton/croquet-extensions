b20ea0670763339b062a0d7419112791fc6da3f0
Zack <zack@ukaton.com>
2020-01-30 08:42:39 -0800

    .

--- file-sharing/extension.js
+++ file-sharing/extension.js
@@ -20,12 +20,63 @@ export default {
                 this.model = model;
 
                 this.client = new WebTorrent();
-                this.client.on("torrent", torrent => {
-                    this.broadcastSigned(this.sessionId, "new-torrent", {
-                        magnetURI : torrent.magnetURI,
-                        stuff : "Hello",
-                    });
+
+                this.client.on("error", error => {
+                    console.error(error);
                 });
+                this.client.on("torrent", t => console.log(t))
+                
+                this.subscribe(this.sessionId, "new-torrent", data => {
+                    const {magnetURI, metadata} = data.data;
+                    const {fromIndex} = data;
+
+                    this.clearUnusedTorrents()
+                        .then(() => {
+                            if(!this.client.get(magnetURI)) {
+                                this.client.add(magnetURI, torrent => {
+                                    torrent.on("done", () => {
+                                        this.dispatchEvent({
+                                            type : "share",
+                                            message : {
+                                                fromIndex,
+                                                metadata,
+                                                files : torrent.files,
+                                            }
+                                        });
+                                    });
+                                });
+                            }
+                        });
+                });
+            }
+
+            share(file, metadata) {
+                new Promise(resolve => {
+                    const torrent = this.client.get(file);
+                    if(torrent)
+                        torrent.destroy(() => resolve());
+                    else
+                        resolve();
+                }).then(() => {
+                    this.clearUnusedTorrents()
+                        .then(() => {
+                            this.client.seed(file, torrent => {
+                                this.broadcastSigned(this.sessionId, "new-torrent", {
+                                    magnetURI : torrent.magnetURI,
+                                    metadata
+                                });
+                            });
+                        });
+                });
+            }
+
+            clearUnusedTorrents() {
+                return Promise.all(this.client.torrents.map(torrent => new Promise(resolve => {
+                    if(torrent.progress == 0)
+                        torrent.destroy(() => resolve())
+                    else
+                        resolve();
+                })))
             }
         }
     }
